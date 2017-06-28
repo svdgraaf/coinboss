cbbf8253b688a2f84a49e3b84858518258d5ae59
Sander van de Graaf <mail@svdgraaf.nl>
2017-06-28 21:51:33 +0200

    added average calculator

--- handler.js
+++ handler.js
@@ -1,16 +1,70 @@
 'use strict';
+var Client = require('coinbase').Client;
+var AWS = require('aws-sdk');
+var moment = require('moment');
+var cloudwatch = new AWS.CloudWatch();
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+var cbClient = new Client({'apiKey': process.env.API_KEY, 'apiSecret': process.env.API_SECRET});
+
+module.exports.updateTransactionPrices = (event, context, callback) => {
+
+  var currencyPair = event.coin + '-' + process.env.CURRENCY;
+
+  cbClient.getBuyPrice({'currencyPair': currencyPair}, function(err, price) {
+    module.exports.putCloudwatchValue('Buy ' + currencyPair, price.data.amount);
+  });
+
+  cbClient.getSellPrice({'currencyPair': currencyPair}, function(err, price) {
+    module.exports.putCloudwatchValue('Sell ' + currencyPair, price.data.amount);
+  });
+};
+
+module.exports.putCloudwatchValue = (what, amount) => {
+  var params = {
+    MetricData: [
+      {
+        MetricName: what,
+        Timestamp: new Date,
+        Value: parseFloat(amount),
+        Unit: 'Count'
+      },
+    ],
+    Namespace: 'Coinboss'
+  };
+  console.log(params);
+  cloudwatch.putMetricData(params, function(err, data){
+    console.log(err, data);
+  });
+}
+
+module.exports.calculateAverage = (event) => {
+  var currencyPair = process.env.CURRENCY + '-' + process.env.CURRENCY;
+
+  var params = {
+    EndTime: new Date,
+    MetricName: 'Sell ' + currencyPair,
+    Namespace: 'Coinboss',
+    Period: event.minutes * 60,
+    StartTime: moment(new Date).subtract(event.minutes, 'm').toDate(),
+    Statistics: ['Average'],
+    Unit: 'Count'
   };
 
-  callback(null, response);
+  cloudwatch.getMetricStatistics(params, function(err, data) {
+    if (err){
+      console.log(err, err.stack); // an error occurred
+    } else {
+      console.log(data);           // successful response
+      module.exports.putCloudwatchValue('ETH ' + event.minutes ' average ', data.Datapoints[0].Average);
+    }
+  });
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+
+module.exports.buyCoins = (event, context, callback) => {}
+module.exports.sellCoins = (event, context, callback) => {}
+
+module.exports.updateThresholds = (event, context, callback) => {
+  module.exports.putCloudwatchValue('Buy At', event.BuyAt);
+  module.exports.putCloudwatchValue('Sell At', event.SellAt);
 };
